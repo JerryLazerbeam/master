b3e491a3eb931c1d138d503d8309c3a318a78d51
Oskar Henze <oskarhenze@Oskars-MacBook-Pro.local>
2026-04-21 13:14:32 +0200

    git och db fix

--- data/db.js
+++ data/db.js
@@ -1,17 +1,20 @@
+const path = require('path');
+const fs = require('fs');
 const Database = require('better-sqlite3');
 
-const db = new Database('./data/freakyfashion.db', {
-  verbose: console.log
-});
+const dbPath = path.join(__dirname, 'freakyfashion.db');
+const sqlPath = path.join(__dirname, 'freakyfashion.sql');
 
-db.prepare('DELETE FROM categories').run();
+const db = new Database(dbPath);
 
-const count = db.prepare('SELECT COUNT(*) as count FROM categories').get();
+const table = db.prepare(`
+  SELECT name FROM sqlite_master WHERE type='table' AND name='categories'
+`).get();
 
-if (count.count === 0) {
-  db.prepare(`
-    INSERT INTO categories (name)
-    VALUES ('Nyheter'), ('Kläder'), ('Accessoarer'), ('Skor')
-  `).run();
+if (!table) {
+  const sql = fs.readFileSync(sqlPath, 'utf8');
+  db.exec(sql);
+  console.log("Database created automatically");
 }
+
 module.exports = db;

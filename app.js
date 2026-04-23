91000be2e8e11d0f9e39a2c1061fb1d76ebabce6
JerryLazerbeam <oskarhenze@hotmail.com>
2026-04-23 15:01:00 +0200

    structure fixed

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-const adminRouter = require('./routes/admin');
+const adminRouter = require('./routes/admin/index');
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler

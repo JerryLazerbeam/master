b1d650f070a446f1ed8c8b87654a6a6a56eff424
JerryLazerbeam <oskarhenze@hotmail.com>
2026-04-27 11:05:26 +0200

    update dynamic prod.card

--- README.md
+++ README.md
@@ -15,6 +15,18 @@ Built using Express.
 
 `npm start`
 
+## Install GIT-LFS for large .jpg files
+
+1. Go to https://git-lfs.com/ download Git LFS and install
+
+2. In terminal run: `git lfs install`
+
+3. Select the files you want Git LFS to manage. run: `git lfs track "*.jpg"`
+
+### Reset Db when working or updating
+
+1. localhost:3000/reset.db
+
 ### Git commit (create a new branch and push branch to Github)
 
 1. `git checkout main`
@@ -26,3 +38,4 @@ Built using Express.
 4. `git add .`
 5. `git commit -m "..."`
 6. `git push -u origin your-branch`
+
--- data/freakyfashion.sql
+++ data/freakyfashion.sql
@@ -1,16 +1,27 @@
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
-  price INTEGER NOT NULL CHECK (price >= 0)
+  price INTEGER NOT NULL CHECK (price >= 0),
+  slug TEXT,
+  description TEXT,
+  image TEXT,
+  brand TEXT
 );
 
-
-INSERT INTO products (name, price)
+INSERT INTO products (name, price, slug, description, image, brand)
 VALUES
-('Svart T-Shirt', 199),
-('Vit T-Shirt', 199);
-
-
+('Freaky Fashion svart t-shirt', 199, 'svart-tshirt', 'Svart t-shirt', '/images/selected/FFsvart.jpg', 'Freaky Fashion'),
+('Freaky Fashion vit t-shirt', 199, 'vit-tshirt', 'Vit t-shirt', '/images/selected/FFvit.jpg', 'Freaky Fashion'),
+('Freaky Fashion off-white t-shirt', 199, 'off-white-pink-tshirt', 'Off-white pink t-shirt', '/images/selected/FFoffwhitepink.jpg', 'Freaky Fashion'),
+('Freaky Fashion beige t-shirt', 199, 'beige-tshirt', 'Beige t-shirt', '/images/selected/FFbeige.jpg', 'Freaky Fashion'),
+('Freaky Fashion grå t-shirt', 199, 'grå-tshirt', 'Grå t-shirt', '/images/selected/FFgrey.jpg', 'Freaky Fashion'),
+('Freaky Fashion grön t-shirt', 199, 'grön-tshirt', 'Grön t-shirt', '/images/selected/FFgreen.jpg', 'Freaky Fashion'),
+('Freaky Fashion navy t-shirt', 199, 'navy-tshirt', 'Navy t-shirt', '/images/selected/FFnavy.jpg', 'Freaky Fashion'),
+('Freaky Fashion pink tattoo t-shirt', 199, 'pink-tattoo-tshirt', 'Pink tattoo t-shirt', '/images/selected/FFpinktattoo.jpg', 'Freaky Fashion'),
+('Freaky Fashion ljusbrun t-shirt', 199, 'ljusbrun-tshirt', 'Ljusbrun t-shirt', '/images/selected/FFljusbrun.jpg', 'Freaky Fashion'),
+('Freaky Fashion pink t-shirt', 199, 'pink-tshirt', 'Pink t-shirt', '/images/selected/FFpink.jpg', 'Freaky Fashion'),
+('Freaky Fashion Knives', 199, 'knives', 'Knives', '/images/selected/FFknives.jpg', 'Freaky Fashion'),
+('Freaky Fashion est.2026', 199, 'est2026', 'Est2026', '/images/selected/FFest2026.jpg', 'Freaky Fashion');
 
 CREATE TABLE categories (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,7 +33,7 @@ VALUES
 ('Nyheter'),
 ('Kläder'),
 ('Accessoarer'),
-('Skor');
+('Skor'); 
 
 --Add a link column to the categories table and update it with appropriate links
 SELECT * FROM categories;
